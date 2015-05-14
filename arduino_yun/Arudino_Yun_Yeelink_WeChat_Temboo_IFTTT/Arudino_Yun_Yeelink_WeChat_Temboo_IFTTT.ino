5aa357bfc765565d997cf3e6f5988a43d1c94ddf
Faweiz <faweiz@gmail.com>
2015-05-14 01:04:03 -0400

    v1.3 Arudino_Yun_Yeelink_WeChat_Temboo/ThingSpeak_IFTTT

--- arduino_yun/Arudino_Yun_Yeelink_WeChat_Temboo_IFTTT/Arudino_Yun_Yeelink_WeChat_Temboo_IFTTT.ino
+++ arduino_yun/Arudino_Yun_Yeelink_WeChat_Temboo_IFTTT/Arudino_Yun_Yeelink_WeChat_Temboo_IFTTT.ino
@@ -1,23 +1,26 @@
 /*
+ Control light or any other applicace from Yeelink, Wechat,Temboo/ThingSpeak of IFTTT by using arduino yun
  
  This sketch connects an 415 MHz to control light using the Yeelink (iOS,Andriod app and web), wechat, ifttt(twitter)
  // https://github.com/faweiz
  // https://portfolium.com/faweiz
  // https://www.linkedin.com/in/faweiz
+ 
+ Version change
  V1.0 created 11, May, 2015 to support Yeelink
  V1.1 update 12 May, 2015 to support wechat(weline)
  V1.2 update 13 May 2015 to support Temboo with twitter to read a tweet trigger by IFTTT when we push a DO button. 
- 
- by Fawei Zhang
+ V1.3 update 14 May 2015 to support ThingSpeak's App "Tweet Control" to read a tweet trigger by IFTTT when we push a DO button. 
+
+ Designer: Fawei Zhang
    
  */
  
-
 // include all Libraries needed:
 #include <Process.h>
 #include <Bridge.h>
 #include <Temboo.h>
-#include "password.h"
+#include "password.h" // Contains Yeelink/Weline/Temboo/Thingspeak account info
 
 // set up net client info:
 const unsigned long postingInterval = 1000;  //delay between updates to emoncms.com
@@ -50,7 +53,7 @@ void loop() {
     lastRequest = now;
   }
 }
-  
+
 void updateData() {
   // convert the readings to a String to send it:
   dataStringTemp = "{\"value\":";
@@ -65,7 +68,6 @@ void sendData_yeelink_weline_temboo()
    // form the string for the API header parameter:
   String apiString = "U-ApiKey: ";
   apiString += APIKEY;
-
   // form the string for the URL parameter:
   String yeelink_url = "http://api.yeelink.net/v1.0/";
   yeelink_url += "device/";
@@ -95,7 +97,6 @@ void sendData_yeelink_weline_temboo()
 //  int i = yeelink.available();
 //  Serial.print(i);
 
-  
 /******************************* Get data from weline (wechat) ********************************/ 
    // form the string for the API header parameter:
   String openID = "Openid: ";
@@ -128,6 +129,15 @@ void sendData_yeelink_weline_temboo()
  // Serial.print(j);
  
  /******************************* Get data from Temboo(Twitter) and IFTTT ********************************/ 
+ /* 
+ // limit the number of call for using temboo
+   int numRuns = 1; // Execution count, so this doesn't run forever
+   int maxRuns = 5; // Maximum number of times the Choreo should be executed
+   // Declare 2 string variable to store the content of tweet posted.   
+    String author; // a String to hold the tweet author's name
+    String tweet; // a String to hold the text of the tweet  
+   if(numRuns <= maxRuns)
+ {
  // Learn more from  https://temboo.com/arduino/yun/read-a-tweet
     Serial.print("\nReading Twitter User Timeline... ");
     TembooChoreo UserTimelineChoreo; // Use the temboo twitter's user time line choreo
@@ -139,12 +149,23 @@ void sendData_yeelink_weline_temboo()
     UserTimelineChoreo.setAccountName(TEMBOO_ACCOUNT);
     UserTimelineChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
     UserTimelineChoreo.setAppKey(TEMBOO_APP_KEY);
- // Set Choreo inputs
+ */ 
+    
+// skip this, In order to reduece the memory usage, I using a profile to store 
+// my Twitter credemtials on Temboo. This means that I can simply reference them 
+// from my sketch without including all the String data that consumes memory
+// Learn more : https://temboo.com/videos#profiles
+// https://temboo.com/library/Library/Twitter/Timelines/UserTimeline/
+/* // Set Choreo inputs
     UserTimelineChoreo.addInput("ScreenName", ScreenName);
     UserTimelineChoreo.addInput("AccessToken", AccessToken);
     UserTimelineChoreo.addInput("AccessTokenSecret", AccessTokenSecret);
     UserTimelineChoreo.addInput("ConsumerSecret", ConsumerSecret);
     UserTimelineChoreo.addInput("ConsumerKey", ConsumerKey);
+*/ 
+/*
+ // Set profile to use for execution
+    UserTimelineChoreo.setProfile("twitter");
  // Identify the Choreo to run
     UserTimelineChoreo.setChoreo("/Library/Twitter/Timelines/UserTimeline");
  // we want the text of the tweet
@@ -153,9 +174,6 @@ void sendData_yeelink_weline_temboo()
     UserTimelineChoreo.addOutputFilter("author", "/[1]/user/screen_name", "Response");   
  // Run the Choreo; when results are available, print them to serial
     UserTimelineChoreo.run();  
- // Declare 2 string variable to store the content of tweet posted.   
-    String author; // a String to hold the tweet author's name
-    String tweet; // a String to hold the text of the tweet  
  // choreo outputs are returned as key/value pairs, delimited with 
  // newlines and record/field terminator characters, for example:
  // Name1\n\x1F
@@ -184,12 +202,50 @@ void sendData_yeelink_weline_temboo()
      Serial.println("Done! Twitter");
      Serial.println("@" + author + " - " + tweet);
      UserTimelineChoreo.close();
-
+ } 
+ */
+ // Since the temboo only allow us to use 1000 Choreo call per month for free plan; 
+ // therefore, I try to use alternative serivice ThingSpeak's app "Tweet Control"
+ // Learn more: https://thingspeak.com/docs/tweetcontrol
+ 
+ /******************************* Get data from ThingSpeak (ifttt/do button/ twitter) ********************************/ 
+   // form the string for the API header parameter:
+  String thingSpeak_api = " ThingSpeak API: ";
+  thingSpeak_api += thingSpeakAPI;
+  
+  // form the string for the URL parameter:
+  String thingSpeak_url = "http://api.thingspeak.com/";
+  thingSpeak_url += "channels/";
+  thingSpeak_url += CHANEELID;
+  thingSpeak_url += "/feed/last?key="; // "Get Last Entry in a Channel Feed" https://thingspeak.com/docs/channels#get_feed
+  thingSpeak_url += thingSpeakAPI;
+  // Send the HTTP PUT request
+    
+  // Is better to declare the Process here, so when the
+  // sendData function finishes the resources are immediately
+  // released. Declaring it global works too, BTW.
+  Process thingSpeak;
+  Serial.print("\nSending thingSpeak data... ");
+  thingSpeak.begin("curl");
+  thingSpeak.addParameter("--request");
+  thingSpeak.addParameter("GET");
+  thingSpeak.addParameter("--data-binary");
+  thingSpeak.addParameter(dataStringTemp);
+  thingSpeak.addParameter("--header");
+  thingSpeak.addParameter(thingSpeak_api);
+  thingSpeak.addParameter(thingSpeak_url);
+  thingSpeak.run();
+  Serial.println("Done! thingSpeak");
+  Serial.println(thingSpeak_url);
+  
+ // int T = thingSpeak.available();
+ // Serial.print(T);
+ 
 /******************************* Printing data   ********************************/ 
-
-  char yee,we;
+  char yee,we,thsp;
   String yeelinkState = "";
   String welineState = "";
+  String thingSpeak_Twitter_State ="";
   
   // If there's incoming data from the net connection,
   // send it out the Serial:
@@ -205,7 +261,7 @@ void sendData_yeelink_weline_temboo()
   Serial.print("yeelinkState value: ");Serial.print(y);
   Serial.println();
   
-   /////////////////////////// Print the weline data  /////////////////////////// 
+  /////////////////////////// Print the weline data  /////////////////////////// 
    while (weline.available()>0) {
     we = weline.read();
     welineState += we;
@@ -215,89 +271,29 @@ void sendData_yeelink_weline_temboo()
   char w = welineState.charAt(welineState.length()- 3);
   Serial.print("welineState value: "); Serial.print(w);
   Serial.println();
- 
-  /////////////////////////// Print the twitter data  /////////////////////////// 
+/* 
+  /////////////////////////// Print the twitter data from temboo  /////////////////////////// 
   Serial.print("Twitter Value: @" + author + " - " + tweet);
   Serial.println();
   char t = tweet.charAt(tweet.length() - 1);
   Serial.print("Twitter value: "); Serial.print(t);
   Serial.println();
- 
- 
- 
-/******************************* Determine the status of the data    ********************************/
-   if(y == '0'&& w == '0' && t == '0' )
-   {  
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,LOW); 
-       Serial.print(" LED OFF");
-   }
-  if(y == '0'&& w == '0' && t =='1')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   if(y == '0'&& w == '1' && t =='0')
-   {  
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   
-   // The controller can't be control the light from both of 2 platform.....
-   // You could control, but it will doing the backward. 
-   // Such as, when y = 1; the w = 0 the light up.and when y = 1, w = 1, the light down etc...
-   if(y == '0'&& w == '1' && t =='1')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   if(y == '1'&& w == '0' && t =='0')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   if(y == '1'&& w == '0' && t =='1')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   if(y == '1'&& w == '1' && t =='0')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,HIGH); 
-       Serial.print(" LED ON");
-   }
-   if(y == '1'&& w == '1' && t =='1')
-   {
-       Serial.println();
-       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
-       Serial.println();
-       digitalWrite(13,LOW); 
-       Serial.print(" LED OFF");
-   }
-   
-/******************************* Determine the status of the data    ********************************/
+*/ 
+
+  /////////////////////////// Print the thingSpeak data  /////////////////////////// 
+  while (thingSpeak.available()) {
+    thsp = thingSpeak.read();
+    thingSpeak_Twitter_State += thsp;
+  }
+  Serial.print("thingSpeak_Twitter State: "); Serial.print(thingSpeak_Twitter_State);
+  Serial.println();
+  char t = thingSpeak_Twitter_State.charAt(thingSpeak_Twitter_State.length()- 3);
+  Serial.print("thingSpeak_Twitter State value: "); Serial.print(t);
+  Serial.println();
+  
+/******************************* Determine the status of the data  (yeelink and wechat only)  ********************************/
 /*
-   if(y == '0'&& w == '0' && )
+   if(y == '0'&& w == '0')
    {  
        Serial.println();
        Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);
@@ -333,8 +329,79 @@ void sendData_yeelink_weline_temboo()
        digitalWrite(13,LOW); 
        Serial.print(" LED OFF");
    }
+ */  
+/********************** Determine the status of the data (yeelink,wechat,(Temboo or Thingspeak),twitter,ifttt) **********************/
+
+   if(y == '0'&& w == '0' && t == '0' )
+   {  
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,LOW); 
+       Serial.print(" LED OFF");
+   }
+  if(y == '0'&& w == '0' && t =='1')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
+   if(y == '0'&& w == '1' && t =='0')
+   {  
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
    
- */
+   // The controller can't be control the light from both of 2 platform.....
+   // You could control, but it will doing the backward. 
+   // Such as, when y = 1; the w = 0 the light up.and when y = 1, w = 1, the light down etc...
+   if(y == '0'&& w == '1' && t =='1')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
+   if(y == '1'&& w == '0' && t =='0')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
+   if(y == '1'&& w == '0' && t =='1')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
+   if(y == '1'&& w == '1' && t =='0')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,HIGH); 
+       Serial.print(" LED ON");
+   }
+   if(y == '1'&& w == '1' && t =='1')
+   {
+       Serial.println();
+       Serial.print("y = "); Serial.print(y);Serial.print(" w = "); Serial.print(w);Serial.print(" t = "); Serial.print(t);
+       Serial.println();
+       digitalWrite(13,LOW); 
+       Serial.print(" LED OFF");
+   }
+
+
    // Ensure the last bit of data is sent.
    Serial.flush(); 
 }
