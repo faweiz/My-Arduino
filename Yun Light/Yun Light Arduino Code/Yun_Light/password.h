30ed79480df1ba4d2b2421e18bfff3bc504994ee
Faweiz <faweiz@gmail.com>
2015-08-29 01:36:42 -0400

    Create password.h
    Project Tutital : https://www.hackster.io/faweiz/arduino_yun_light

---
+++ Yun Light/Yun Light Arduino Code/Yun_Light/password.h
@@ -0,0 +1,29 @@
+/* Enter your Yeelink account SDK info */
+#define APIKEY              "Your yelink api key"          		 // replace your yelink api key here
+#define DEVICEID            "Your yelink Device ID"              // Device ID
+#define SENSORID            "Your yelink Sensor ID"              // Sensor ID for lightController
+#define USERAGENT           "Your yelink project name"           // user agent is the project name
+
+/* Enter your weline (wechat) info */
+#define openAPI           	"Your weline openAPI key"            // replace your weline API key here
+#define welineDEVICEID      "Your weline DEVICE ID"              // Weline DEVICE ID
+
+/* Enter your Temboo account info */
+#define TEMBOO_ACCOUNT       "Your Temboo account name "         // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME  "Your Temboo app key name"          // your Temboo app key name
+#define TEMBOO_APP_KEY       "Your Temboo app key"         		 // your Temboo app key
+
+/* Enter your ThingSpeak info */
+#define thingSpeakAPI        "Your ThingSpeak API key"           // replace your ThingSpeak API key here
+#define CHANEELID            "Your ThingSpeak ID"                // ThingSpeak ID
+
+/* Enter Blynk info */
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "Your Blynk Auth";
+
+/* Enter DeviceHub info */
+#define DeviceApiKey    "Your DeviceHub API KEY"           		// paste_your_API_KEY_here
+#define PROJECT_ID      "Your DeviceHub PROJECT ID"             // paste_your_PROJECT_ID_here
+#define ACTUATOR_NAME   "Your DeviceHub ACTUATORNAME"           // paste_your_ACTUATOR_NAME_here
+#define DEVICE_UUID     "Your DeviceHub DEVICE UUID"            // paste_your_DEVICE_UUID_here
